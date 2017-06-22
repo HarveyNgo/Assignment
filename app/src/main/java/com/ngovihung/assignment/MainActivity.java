6f7ce86c9f5705bb56d642c6773b8178b6fce104
hung <ngo.vi.hung@inte.co.jp>
2017-06-22 17:45:36 +0700

    update

--- app/src/main/java/com/ngovihung/assignment/MainActivity.java
+++ app/src/main/java/com/ngovihung/assignment/MainActivity.java
@@ -2,6 +2,11 @@ package com.ngovihung.assignment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.DataSet;
@@ -10,6 +15,7 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -17,11 +23,21 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+import static com.sothree.slidinguppanel.SlidingUpPanelLayout.PanelState.ANCHORED;
+import static com.sothree.slidinguppanel.SlidingUpPanelLayout.PanelState.COLLAPSED;
+import static com.sothree.slidinguppanel.SlidingUpPanelLayout.PanelState.DRAGGING;
+import static com.sothree.slidinguppanel.SlidingUpPanelLayout.PanelState.EXPANDED;
+import static com.sothree.slidinguppanel.SlidingUpPanelLayout.PanelState.HIDDEN;
+
+public class MainActivity extends AppCompatActivity implements  SlidingUpPanelLayout.PanelSlideListener, View.OnClickListener {
 
     LineChart lineChart;
 
+    ViewGroup activity_main_fl_options;
+    ViewGroup option_daily_monthly_quarter;
     ArrayList<Portfolio>  portfolios;
+    TextView activity_main_tv_option;
+    SlidingUpPanelLayout activity_main_content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +45,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Application.setActiveActivity(this);
         Application.setContext(this);
-        lineChart = (LineChart) findViewById(R.id.chart);
+        lineChart = (LineChart) findViewById(R.id.activity_main_chart);
+        activity_main_fl_options = (ViewGroup) findViewById(R.id.activity_main_fl_options);
+        View optionLayout = LayoutInflater.from(this).inflate(R.layout.option,null);
+        option_daily_monthly_quarter = (ViewGroup) optionLayout.findViewById(R.id.option_daily_monthly_quarter);
+        activity_main_tv_option = (TextView) findViewById(R.id.activity_main_tv_option);
+        activity_main_tv_option.setOnClickListener(this);
+        activity_main_content = (SlidingUpPanelLayout) findViewById(R.id.activity_main_content);
         portfolios = new ArrayList<>();
         getData();
         //setChart();
@@ -146,5 +168,43 @@ public class MainActivity extends AppCompatActivity {
         lineChart.setData(data);
         lineChart.notifyDataSetChanged();
         lineChart.invalidate();
+    }
+
+    @Override
+    public void onPanelSlide(View panel, float slideOffset) {
+
+    }
+
+    @Override
+    public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
+        if (panel.findViewById(R.id.activity_main_fl_options).equals(activity_main_fl_options)) {
+            if ((previousState == ANCHORED || previousState == EXPANDED) && newState == HIDDEN)
+                activity_main_fl_options.removeAllViews();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.activity_main_tv_option:
+                openTableOptions(option_daily_monthly_quarter);
+        }
+    }
+
+    public void openTableOptions(View view) {
+        if (activity_main_content.getPanelState() == ANCHORED || activity_main_content.getPanelState() == DRAGGING)
+            return;
+
+        activity_main_fl_options.removeAllViews();
+        if (view instanceof View)
+            activity_main_fl_options.addView((View) view);
+        activity_main_content.setPanelState(ANCHORED);
+    }
+
+    public void closeTableOptions() {
+        if (activity_main_content.getPanelState() == HIDDEN || activity_main_content.getPanelState() == COLLAPSED || activity_main_content.getPanelState() == DRAGGING)
+            return;
+
+        activity_main_content.setPanelState(HIDDEN);
     }
 }
