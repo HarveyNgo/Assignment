c5aaab4054ca657aa896ca96e6f83422c2891f44
hung <ngo.vi.hung@inte.co.jp>
2017-06-22 15:43:40 +0700

    update

--- app/src/main/java/com/ngovihung/assignment/MainActivity.java
+++ app/src/main/java/com/ngovihung/assignment/MainActivity.java
@@ -68,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }
                     }
+                    //
                     portfolios.add(p);
                 }
             }
